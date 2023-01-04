7a2d5077561843bb8c301bd8ac98274888999f86
williamtyn <william@tyner.se>
2023-01-04 14:55:12 +0000

    add hypothesis and validation page

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.project_summary import project_summary_body
 from app_pages.price_study import correlation_study
+from app_pages.hypothesis_validation import hypothesis_validation_body
 
 app = MultiPage(app_name= "Heritage Housing Issues") # Create an instance of the app 
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", project_summary_body)
 app.add_page("House Prices Correlation Study", correlation_study)
+app.add_page("Hypothesis and Validation", hypothesis_validation_body)
 
 app.run()
---
+++ app_pages/hypothesis_validation.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def hypothesis_validation_body():
+    st.write("### Project Hypothesis and Validation")
