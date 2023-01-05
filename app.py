bc2d535ef0459f67427fc2f0728edcbc649d68ab
williamtyn <william@tyner.se>
2023-01-05 11:43:07 +0000

    add predict sale price page to app pages

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.project_summary import project_summary_body
 from app_pages.price_study import correlation_study
 from app_pages.hypothesis_validation import hypothesis_validation_body
+from app_pages.predict_sale_price import predict_sale_price_body
 
 app = MultiPage(app_name= "Heritage Housing Issues") # Create an instance of the app 
 
@@ -12,5 +13,6 @@ app = MultiPage(app_name= "Heritage Housing Issues") # Create an instance of the
 app.add_page("Quick Project Summary", project_summary_body)
 app.add_page("House Prices Correlation Study", correlation_study)
 app.add_page("Hypothesis and Validation", hypothesis_validation_body)
+app.add_page("House Sale Price Predictor", predict_sale_price_body)
 
 app.run()
---
+++ app_pages/predict_sale_price.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def predict_sale_price_body():
+    st.write("### House Sale Price Predictor")
+    st.info(
+            f"* Predicting the house sales price for our client for the inherited houses, and also any other house in Ames, Iowa. \n"
+            )
