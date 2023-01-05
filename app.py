9bc6daae8545ec395ea1ac0a9912808bb9405cc7
williamtyn <william@tyner.se>
2023-01-05 14:04:46 +0000

    add ml prediction page

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ from app_pages.project_summary import project_summary_body
 from app_pages.price_study import correlation_study
 from app_pages.hypothesis_validation import hypothesis_validation_body
 from app_pages.predict_sale_price import predict_sale_price_body
+from app_pages.ml_prediction import ml_prediction_body
 
 app = MultiPage(app_name= "Heritage Housing Issues") # Create an instance of the app 
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", project_summary_body)
 app.add_page("House Prices Correlation Study", correlation_study)
+app.add_page("Predict House Price", ml_prediction_body)
 app.add_page("Hypothesis and Validation", hypothesis_validation_body)
 app.add_page("House Sale Price Predictor", predict_sale_price_body)
+
 
 app.run()
---
+++ app_pages/ml_prediction.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def ml_prediction_body():
+
+    st.write("### Predict House Sale Price")
