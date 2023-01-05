7620e2a33e7c2738f27726ccb31e2a2fd49478ef
williamtyn <william@tyner.se>
2023-01-05 07:42:46 +0000

    add hypothesis and validation to page

--- app_pages/hypothesis_validation.py
+++ app_pages/hypothesis_validation.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 def hypothesis_validation_body():
     st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"* Houses with more GrLivArea should have higher SalesPrice.\n\n"
+        f"The correlation study at House Prices Study supports that. \n"
+
+        f"* The younger the house is YearBuilt the better the OverallCond is of the house.  \n\n"
+        f"The correlation feature study did not support that hypothesis since the correlation was poor.\n"
+        f"The correlation between the quality and yearbuilt was higher but not strong.\n"
+    )
