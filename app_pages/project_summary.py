b9ac9c604412fe4b88ea3437ddf32ffce8d07e46
williamtyn <william@tyner.se>
2023-01-06 19:12:57 +0000

    fix spelling on project summary page

--- app_pages/project_summary.py
+++ app_pages/project_summary.py
@@ -13,21 +13,21 @@ def project_summary_body():
 
     st.write(
         f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/williamtyn/heritage-housing-issues/blob/main/jupyter_notebooks/Predict%20Sale%20Price.ipynb).")
+        f"[Project README file](https://github.com/williamtyn/heritage-housing-issues).")
     
 
     # copied from README file - "Business Requirements" section
     st.success(
         f"**The project has 2 business requirements:**\n"
-        f"* 1 - The client is interested in discovering how the house attributes correlate with the sale price. \n"
+        f"* 1 : The client is interested in discovering how the house attributes correlate with the sale price. \n"
         f" Therefore, the client expects data visualizations of the correlated variables against the sale price to show that.\n "
         
-        f"* 2 - The client is interested in predicting the house sales price from her four inherited houses, and any other house in Ames, Iowa. "
+        f"* 2 : The client is interested in predicting the sale price of her four inherited houses and any other house in Ames, Iowa. "
         )
 
     st.write(
         f"**Used Data**\n"
-        f"* We have used a publicly available data from. \n"
+        f"* We have used publicly available data from \n"
         f"[Kaggle.com](https://www.kaggle.com/datasets/codeinstitute/" +
         f"housing-prices-data)"
         )
